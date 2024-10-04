73c7248afb773202ab8ba2fc389b412db5ffbaf8
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-10-04 19:50:29 -0300

    adjusted home page

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import { useData } from '../Context/DataContext';
+import { useData } from "../Context/DataContext";
 
-const Resumo = () =>
-{
+const Resumo = () => {
   const { data } = useData();
-  return (
-    <section className='resumo'>
-      RESUMO
-    </section>
-  )
-}
 
-export default Resumo
+  if (data === null) return null;
+
+  const calcularTotal = (status: string | null) =>
+    data
+      .filter((item) =>
+        status ? item.status === status : item.status !== "falha"
+      )
+      .reduce((acc, item) => acc + item.preco, 0)
+      .toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+
+  return (
+    <section>
+      <div className="resumo flex mb">
+        {[
+          { titulo: "Vendas", status: null },
+          { titulo: "Recebido", status: "pago" },
+          { titulo: "Processando", status: "processando" },
+        ].map(({ titulo, status }) => (
+          <div className="box" key={titulo}>
+            <h2>{titulo}</h2>
+            <span>{calcularTotal(status)}</span>
+          </div>
+        ))}
+      </div>
+      <div className="box mb">
+Graficos
+      </div>
+    </section>
+  );
+};
+
+export default Resumo;
