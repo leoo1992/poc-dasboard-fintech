b487cd96f5cc026bcf286ef072f6ee496e024360
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-10-04 19:49:06 -0300

    add sidebar

--- src/Components/SideNav.tsx
+++ src/Components/SideNav.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
+import resumoICON from "../assets/icons/resumo.svg";
+import vendasICON from "../assets/icons/vendas.svg";
+import FintechSVG from "../assets/FintechSVG";
 
 const SideNav = () => {
   return (
-    <div>
-      SIDENAV
-    </div>
-  )
-}
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Logo" />
+      <ul>
+        <li>
+          <span>
+            <img src={resumoICON} alt="" />
+          </span>
+          <NavLink to="/">Resumo</NavLink>
+        </li>
+        <li>
+          <span>
+            <img src={vendasICON} alt="" />
+          </span>
+          <NavLink to="/vendas">Vendas</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default SideNav
+export default SideNav;
