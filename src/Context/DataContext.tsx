44ecb0b5ae826ae7cb98307469c3c5b8665e7874
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-10-04 19:48:38 -0300

    adjust context

--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import useFetch from "../Hooks/useFetch";
 
-type IVenda = {
+export type IVenda = {
   id: string;
   nome: string;
   preco: number;
@@ -15,6 +15,10 @@ type IDataContext = {
   data: IVenda[] | null;
   loading: boolean;
   error: string | null;
+  inicio: string;
+  final: string;
+  setInicio: React.Dispatch<React.SetStateAction<string>>;
+  setFinal: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const DataContext = React.createContext<IDataContext | null>(null);
@@ -28,13 +32,29 @@ export const useData = () => {
   return context;
 };
 
+function getDateDaysAgo(n: number): string {
+  const date = new Date();
+  date.setDate(date.getDate() - n);
+
+  const dd = String(date.getDate()).padStart(2, "0");
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const yyyy = date.getFullYear();
+
+  return `${yyyy}-${mm}-${dd}`;
+}
+
 export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
+  const [inicio, setInicio] = React.useState(getDateDaysAgo(30));
+  const [final, setFinal] = React.useState(getDateDaysAgo(0));
+
   const { data, loading, error } = useFetch<IVenda[]>(
-    "https://data.origamid.dev/vendas/"
+    `https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`
   );
 
   return (
-    <DataContext.Provider value={{ data, loading, error }}>
+    <DataContext.Provider
+      value={{ data, loading, error, inicio, final, setInicio, setFinal }}
+    >
       {children}
     </DataContext.Provider>
   );
